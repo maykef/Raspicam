886c994303b3b0ac2dd6bf38225efe6b47d20f78
Mayke De Freitas <maykef@gmail.com>
2018-10-29 18:27:16 +0000

    Update numpy_array_save1.py

--- numpy_array_save1.py
+++ numpy_array_save1.py
@@ -1,39 +1,17 @@
-from time import sleep
-from datetime import datetime
+import time
 import picamera
 import picamera.array
 import numpy as np
 import cv2
-import os,glob
 
-i=0
-
-for dir in glob.glob('Pictures'):
-  if os.path.isdir(dir):
-      latest_folder = os.path.getctime(dir)
-      folder_time = datetime.fromtimestamp(latest_folder).strftime('%Y-%m-%d_%H:%M:%S')
-      print('Joining '+folder_time+' folder')
-      newDirName= os.path.join(folder_time)
-
-camera = picamera.PiCamera(1)
-
-with camera:
-    camera.sensor_mode = 5
-    camera.iso = 100
-    sleep(2)
-    camera.shutter_speed = camera.exposure_speed
-    camera.exposure_mode = 'off'
-    g = camera.awb_gains
-    camera.awb_mode = 'off'
-    camera.awb_gains = g
-    while True:
-        print('Capturing images...')
-        output = picamera.array.PiBayerArray(camera)
-        result = camera.capture(output, 'jpeg', bayer=True)
-        debase = (output.array.astype(np.uint8))
-        n = debase[:,:,0];
-        imageName = os.path.join('Pictures',newDirName,'IMG_{:04d}_4.tiff'.format(i))
-        cv2.imwrite(imageName,n);
-        print('Done!')
-        sleep(2)
-        i=i+1
+with picamera.PiCamera(1) as camera:
+    with picamera.array.PiBayerArray(camera) as stream:
+        camera.capture(stream, 'jpeg', bayer=True)
+        #output = (stream.demosaic() >> 2).astype(np.uint8)
+        output = (output.array.astype(np.uint8))
+        b = output[:,:,0]
+        g = output[:,:,1]
+        r = output[:,:,2]
+        cv2.imwrite('image_1.tiff',b)
+        cv2.imwrite('image_2.tiff',g)
+        cv2.imwrite('image_3.tiff',r)
