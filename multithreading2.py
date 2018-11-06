1408a05d64e1d7e60725e68c20d0993f0ed896e1
Mayke De Freitas <maykef@gmail.com>
2018-11-06 10:16:34 +0000

    Update multithreading2.py

--- multithreading2.py
+++ multithreading2.py
@@ -1,5 +1,4 @@
-import os, random
-from multiprocessing import Process, Queue
+from multiprocessing import Process, Pipe
 from time import sleep
 from datetime import datetime
 import picamera
@@ -8,7 +7,8 @@ import numpy as np
 import cv2
 import os,glob
 
-def new_folder(q):
+
+def new_folder():
     imagePath = os.path.join('.','Pictures')
     if os.path.exists(imagePath):
         timestr = datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
@@ -18,43 +18,49 @@ def new_folder(q):
         print('Creating '+newDirName+' folder')
         return new_folder
 
-def start_cam1(q):
+def start_cam1(conn):
     camera = picamera.PiCamera(1)
     camera.sensor_mode = 5
     camera.iso = 100
-    sleep(1.2)
+    sleep(0.6)
     camera.shutter_speed = camera.exposure_speed
     camera.exposure_mode = 'off'
     g = camera.awb_gains
     camera.awb_mode = 'off'
     camera.awb_gains = g
+    camera.exif_tags['IFD0.Copyright'] = 'Copyright (c) 2013 Agrosight Ltd'
+    camera.exif_tags['EXIF.ImageUniqueID'] = '70995432222RE'
     print('Starting capture')
     output = picamera.array.PiBayerArray(camera)
     result = camera.capture(output, 'jpeg', bayer=True)
-    debase = (output.demosaic() >> 2).astype(np.uint8)
-    q.put(debase)
+    #debase = (output.demosaic() >> 2).astype(np.uint8)
+    debase = ((output.array >>2).astype(np.uint8))
+    conn.send(debase)
 
 
-def start_cam2(q):
+def start_cam2(conn):
     camera = picamera.PiCamera(0)
     camera.sensor_mode = 5
     camera.iso = 100
-    sleep(1.2)
+    sleep(0.6)
     camera.shutter_speed = camera.exposure_speed
     camera.exposure_mode = 'off'
     g = camera.awb_gains
     camera.awb_mode = 'off'
     camera.awb_gains = g
+    camera.exif_tags['IFD0.Copyright'] = 'Copyright (c) 2013 Agrosight Ltd'
+    camera.exif_tags['EXIF.ImageUniqueID'] = '70995432222RE'
     print('Starting capture')
     output = picamera.array.PiBayerArray(camera)
     result = camera.capture(output, 'jpeg', bayer=True)
-    debase = (output.demosaic() >> 2).astype(np.uint8)
-    q.put(debase)
+    #debase1 = (output.demosaic() >> 2).astype(np.uint8)
+    debase = ((output.array >>2).astype(np.uint8))
+    conn.send(debase)
 
 
-def save_pic1(q):
-    debase=q.get()
-    i=0
+def save_pic1(conn):
+    dt = datetime.now().strftime("%M_%S")
+    debase=conn.recv()
     b = debase[:,:,0];
     g = debase[:,:,1];
     r = debase[:,:,2];
@@ -63,48 +69,44 @@ def save_pic1(q):
             latest_folder = os.path.getctime(dir)
             folder_time = datetime.fromtimestamp(latest_folder).strftime('%Y-%m-%d_%H:%M:%S')
             newDir= os.path.join(folder_time)
-            imageName1 = os.path.join('Pictures',newDir,'IMG_{:04d}_1.tiff'.format(i))
-            imageName2 = os.path.join('Pictures',newDir,'IMG_{:04d}_2.tiff'.format(i))
-            imageName3 = os.path.join('Pictures',newDir,'IMG_{:04d}_3.tiff'.format(i))
+            imageName1 = os.path.join('Pictures',newDir,'IMG_'+dt+'_1''.tiff')
+            imageName2 = os.path.join('Pictures',newDir,'IMG_'+dt+'_2''.tiff')
+            imageName3 = os.path.join('Pictures',newDir,'IMG_'+dt+'_3''.tiff')
             cv2.imwrite(imageName1,b);
             cv2.imwrite(imageName2,g);
             cv2.imwrite(imageName3,r);
             print('Done!')
-            i=i+1
             return save_pic1
 
-def save_pic2(q):
-    debase=q.get()
-    i=0
-    n = debase[:,:,0];
+def save_pic2(conn):
+    dt = datetime.now().strftime("%M_%S")
+    debase1=conn.recv()
+    n = debase1[:,:,0];
     for dir in glob.glob('Pictures'):
         if os.path.isdir(dir):
             latest_folder = os.path.getctime(dir)
             folder_time = datetime.fromtimestamp(latest_folder).strftime('%Y-%m-%d_%H:%M:%S')
             newDir= os.path.join(folder_time)
-            imageName = os.path.join('Pictures',newDir,'IMG_{:04d}_4.tiff'.format(i))
+            imageName = os.path.join('Pictures',newDir,'IMG_'+dt+'_4''.tiff')
             cv2.imwrite(imageName,n);
             print('Done!')
-            i=i+1
             return save_pic2
 
 
 if __name__ == '__main__':
-    q = Queue()
-    print('pushing items to queue:')
-    a = Process(target=new_folder, args=(q,))
-    a.start()
-    a.join()
+    new_folder()
+    start_conn, save_conn = Pipe()
+    start1_conn, save2_conn = Pipe()
     while True:
-        b = Process(target=start_cam1, args=(q,))
-        c = Process(target=save_pic1, args=(q,))
-        d = Process(target=start_cam2, args=(q,))
-        e = Process(target=save_pic2, args=(q,))
-        b.start()
-        d.start()
-        c.start()
-        e.start()
-        b.join()
-        d.join()
-        c.join()
-        e.join()
+        start = Process(target=start_cam1, args=(start_conn,))
+        save = Process(target=save_pic1, args=(save_conn,))
+        start1 = Process(target=start_cam2, args=(start1_conn,))
+        save2 = Process(target=save_pic2, args=(save2_conn,))
+        start.start()
+        save.start()
+        start1.start()
+        save2.start()
+        start.join()
+        save.join()
+        start1.join()
+        save2.join()
