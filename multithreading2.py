a50131e90d3e6480f71d7486b374483ca54bc835
Mayke De Freitas <maykef@gmail.com>
2018-11-17 08:22:02 +0000

    Update multithreading2.py
    Adding comments to script

--- multithreading2.py
+++ multithreading2.py
@@ -23,6 +23,7 @@ def new_folder():
 def start_cam1(conn):
     """Starts camera cs 0 in the Compute Module"""
     camera = picamera.PiCamera(0)
+    """Let's capture images with equal parameters"""
     camera.sensor_mode = 1
     camera.iso = 100
     camera.shutter_speed = camera.exposure_speed
@@ -31,15 +32,21 @@ def start_cam1(conn):
     camera.awb_mode = 'off'
     camera.awb_gains = g
     print('Starting capture')
+    """Capture raw Bayer data"""
     output = picamera.array.PiBayerArray(camera)
     camera.capture(output, 'jpeg', bayer=True)
+    """Let's save the raw Bayer pattern into an array,\
+    shift pixels by 2-bits, and save image as uint8"""
     debase = ((output.array >>2).astype(np.uint8))
+    """Now let's save the array and share it with our \
+    process save_pic1"""
     conn.send(debase)
 
 
 def start_cam2(conn):
     """Starts camera cs 1 in the Compute Module"""
     camera = picamera.PiCamera(1)
+    """Let's capture images with equal parameters"""
     camera.sensor_mode = 1
     camera.iso = 100
     camera.shutter_speed = camera.exposure_speed
@@ -48,30 +55,45 @@ def start_cam2(conn):
     camera.awb_mode = 'off'
     camera.awb_gains = g
     print('Starting capture')
+    """Capture raw Bayer data"""
     output = picamera.array.PiBayerArray(camera)
     camera.capture(output, 'jpeg', bayer=True)
+    """Let's save the raw Bayer pattern into an array,\
+    shift pixels by 2-bits, and save image as uint8"""
     debase = ((output.array >>2).astype(np.uint8))
+    """Now let's save the array and share it with our \
+    process save_pic2"""
     conn.send(debase)
 
 
 def save_pic1(conn):
-    """Saves RGB in three different channels"""
+    """Let's call datetime so our image name includes it"""
     dt = datetime.now().strftime("%M_%S.%f")[:-3]
+    """Receive the array from start_cam1"""
     debase=conn.recv()
+    """Deconstruct the array into three (red, green and blue) channels"""
     r = debase[:,:,0]
     g = debase[:,:,1]
     b = debase[:,:,2]
+    """Let's join the folder created by new_folder"""
     for dir in glob.glob('Pictures'):
         if os.path.isdir(dir):
             latest_folder = os.path.getctime(dir)
             folder_time = datetime.fromtimestamp(latest_folder).strftime('%Y-%m-%d_%H:%M:%S')
             newdir = os.path.join(folder_time)
+            """Prepare the new name for each image"""
             imagename1 = os.path.join('Pictures', newdir, 'IMG_'+dt+'_1''.tiff')
             imagename2 = os.path.join('Pictures', newdir, 'IMG_'+dt+'_2''.tiff')
             imagename3 = os.path.join('Pictures', newdir, 'IMG_'+dt+'_3''.tiff')
+            """Now demosaic the images using opencv; bear in mind that \
+            our Bayer pattern is RGGB, so the second row and columns \
+            are GR"""
             r = cv2.cvtColor(r, cv2.COLOR_BayerGR2RGB)
             g = cv2.cvtColor(g, cv2.COLOR_BayerGR2RGB)
             b = cv2.cvtColor(b, cv2.COLOR_BayerGR2RGB)
+            """BayerGR2RGB will create an image with three channels, \
+            so we need to transform them into single channel images \
+            again"""
             cv2.imwrite(imagename1, r[:,:,0])
             cv2.imwrite(imagename2, g[:,:,1])
             cv2.imwrite(imagename3, b[:,:,2])
@@ -79,17 +101,27 @@ def save_pic1(conn):
             return save_pic1
 
 def save_pic2(conn):
-    """Saves the NIR band in one channel"""
+    """Let's call datetime so our image name includes it"""
     dt = datetime.now().strftime("%M_%S.%f")[:-3]
+    """Receive the array from start_cam1"""
     debase1=conn.recv()
+    """Deconstruct the array into one (NIR) channel"""
     n = debase1[:, :, 0]
+    """Let's join the folder created by new_folder"""
     for dir in glob.glob('Pictures'):
         if os.path.isdir(dir):
             latest_folder = os.path.getctime(dir)
             folder_time = datetime.fromtimestamp(latest_folder).strftime('%Y-%m-%d_%H:%M:%S')
             newdir = os.path.join(folder_time)
+            """Prepare the new name for each image"""
             imagename = os.path.join('Pictures',newdir,'IMG_'+dt+'_4''.tiff')
+            """Now demosaic the image using opencv; bear in mind that \
+            our Bayer pattern is RGGB, so the second row and columns \
+            are GR"""
             n = cv2.cvtColor(n, cv2.COLOR_BayerGR2RGB)
+            """BayerGR2RGB will create an image with three channels, \
+            so we need to transform them into a single channel image \
+            again"""
             cv2.imwrite(imagename, n[:,:,0])
             print('Done!')
             return save_pic2
